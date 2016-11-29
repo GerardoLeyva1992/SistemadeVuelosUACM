21c12d1a8f5b0c1292f142212a760c58bd0cbf00
Gera <Gera@10.0.13.131>
2016-11-28 18:06:43 -0600

    index

--- src/main/java/edu/uacm/controller/AltaController.java
+++ src/main/java/edu/uacm/controller/AltaController.java
@@ -1,15 +1,16 @@
 package edu.uacm.controller;
 
-//import java.io.IOException;
+import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//import edu.uacm.domain.Aeropuerto;
+import edu.uacm.domain.Aeropuerto;
 import edu.uacm.domain.AeropuertoRepository;
 
 @Controller
@@ -26,7 +27,7 @@ public class AltaController {
 		log.debug("--------Estoy entrando a alta()------");
 		return "redirect:/alta.html";
 	}
-	/*
+	
 	@RequestMapping(value="/dandoAlta", method=RequestMethod.POST)
 	public String darAlta(@ModelAttribute Aeropuerto ae) throws IOException{
 		log.debug("-----Dando de alta: -------");
@@ -34,6 +35,6 @@ public class AltaController {
 		aer.save(ae);
 	    return "redirect:/exitoAlta.html";
 	}
-	*/
+	
 	
 }
